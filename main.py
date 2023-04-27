a04dcc283966c8c825673c63ece813c7e934a821
Akun MP Sementara <hendrakaka10@gmail.com>
2023-04-27 15:19:38 +0700

    perulangan dengan while untuk jumlah buku tertentu, contoh 10

---
+++ main.py
@@ -0,0 +1,11 @@
+# Percabangan
+jumlah_botol_susu = 0
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("budi pulang ke rumah")
+print("budi menyampaikan hasilnya kepada ibu")

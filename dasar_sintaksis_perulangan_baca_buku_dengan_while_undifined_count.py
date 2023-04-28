bb12fc54bc5e369ada2e6e79fe7afa02bb3966e8
Akun MP Sementara <hendrakaka10@gmail.com>
2023-04-28 19:12:44 +0700

    perulangan dengan while undifined count

---
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undifined_count.py
@@ -0,0 +1,20 @@
+"""
+Program Perulangan Baca Buku dengan while sampai paham
+"""
+
+jumlah_buku = 10
+total_jumlah_baca = 0
+print('Ibu berkata,"Baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
